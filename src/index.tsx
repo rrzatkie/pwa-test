0bd0b5a97a2e58df38f7e6aacf93a2fb484158e0
Rafal Rzatkiewicz <rrzatkiewicz@gmv.com>
2021-03-22 10:03:03 +0100

    second commit2

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorkerRegistration';
+import * as serviceWorker from './service-worker-registration';
 
 ReactDOM.render(
     <React.StrictMode>
